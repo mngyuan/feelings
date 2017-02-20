99aa7ed610d2f122e56f33c0d8fe4bcc7de4c83d
Kevin Lee <kevinmlee.x@gmail.com>
2017-02-19 16:45:59 -0800

    randomly and stably pick words

--- src/FeelingsInput.js
+++ src/FeelingsInput.js
@@ -9,29 +9,50 @@ class FeelingsInput extends Component {
     this.state = {
       text: '',
       suggestionText: '',
+      words: [],
     };
   }
 
   onChange = (e) => {
     const text = e.target.value;
     const words = text.match(MATCH);
-    const lastTypedWord = words.slice(-1)[0];
+    if (words
+        && this.state.words
+        && words.length === this.state.words.length
+        && this.state.words.reduce((agg, cur, i) => agg && words[i] === cur)) {
+      // Same words still
+      this.setState({
+        text,
+        words,
+      });
+    } else {
+      this.onWordsChange(text, words);
+    }
+  }
+
+  onWordsChange = (text, words) => {
+    const lastTypedWord = words ? words.slice(-1)[0] : '';
     const lastWord = getLastCompleteWord(text, this.props.transitions);
     let suggestion = '';
     let suggestionText = '';
     if (this.props.transitions[lastTypedWord]) {
       // happy birthday
-      suggestion = getPossibleWords('', this.props.transitions[lastWord])[0];
+      suggestion = pickWord(getPossibleWords('', this.props.transitions[lastWord]));
       suggestionText = `${text} ${suggestion}`;
     } else {
       // happy birthd
-      suggestion = getPossibleWords(lastTypedWord, this.props.transitions[lastWord])[0];
-      suggestionText = `${text.slice(0, -lastTypedWord.length)}${suggestion}`;
+      suggestion = this.props.transitions[lastWord]
+        ? pickWord(getPossibleWords(lastTypedWord, this.props.transitions[lastWord]))
+        : null;
+      suggestionText = suggestion
+        ? `${text.slice(0, -lastTypedWord.length)}${suggestion}`
+        : text;
     }
     console.log('lastTypedWord', lastTypedWord, 'lastWord', lastWord, 'suggestion', suggestion, 'suggestionText', suggestionText);
     this.setState({
       text,
       suggestionText,
+      words,
     });
   }
 
@@ -66,6 +87,20 @@ export function getPossibleWords(s, transitions) {
   return Object.entries(transitions)
     // .filter(entry => s === ' ' || entry[0].indexOf(s) > -1)
     .filter(entry => s === ' ' || entry[0].startsWith(s))
-    .sort((a,b) => b[1].count - a[1].count)
-    .map(entry => entry[0]);
+    .sort((a,b) => b[1].count - a[1].count);
+    // .map(entry => entry[0]);
+}
+
+export function pickWord(transitions) {
+  let curRandom = Math.random();
+  let total = 0;
+  for (let [option, info] of transitions) {
+    total += info.count;
+  }
+  for (let [option, info] of transitions) {
+    curRandom -= info.count / total;
+    if (curRandom < 0) {
+      return option;
+    }
+  }
 }
